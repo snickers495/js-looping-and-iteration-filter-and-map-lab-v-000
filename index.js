c7e2242744f3bdb399d9401654cef85c2fd034dd
Lisa Lee <snickers495@gmail.com>
2018-01-13 19:12:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
    return arr.filter(person => person.revenue > money);
  }
  function driverNamesWithRevenueOver(arr, money) {
-
+   const newArr = arr.filter(person => person.revenue > money);
  }
