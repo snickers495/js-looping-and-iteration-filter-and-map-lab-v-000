6bf2b75e206d2ef185a058efb41aec7e6a1fedae
Lisa Lee <snickers495@gmail.com>
2018-01-13 19:14:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,7 @@
    const newArr = arr.filter(person => person.revenue > money);
    return newArr.map(person => person.name);
  }
- function exactMatch(name, obj)
+ function exactMatch(arr, obj) {
+   const key = obj.key
+   return arr.filter(person => )
+ }
